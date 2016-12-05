7a1ce63584f109521d344bd53d7964b72ab50f5a
Mark Timbol <mark.timbol@hotmail.com>
2016-12-05 17:51:18 +0400

    Send *real* email

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'info@citycard.me',
+        'name' => config('app.name'),
     ],
 
     /*
