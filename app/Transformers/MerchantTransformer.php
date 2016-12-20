f38ca9990f1a36c7913ee0f6c03b72e89f293a64
Mark Timbol <mark.timbol@hotmail.com>
2016-12-20 15:44:34 +0400

    Do not show the current outlet on merchant outlets list

--- app/Transformers/MerchantTransformer.php
+++ app/Transformers/MerchantTransformer.php
@@ -7,14 +7,17 @@ use Themsaid\Transformers\AbstractTransformer;
 
 class MerchantTransformer extends AbstractTransformer
 {
-    public function transformModel(Model $merchant)
+    public function transformModel(Model $item)
     {
-    	$output = array_only($merchant->toArray(), [
+    	$output = array_only($item->merchant->toArray(), [
     		'id', 'name', 'logo'
     	]);
 
-    	if( $this->isRelationshipLoaded($merchant, 'outlets') )  {            
-    		$output['outlets'] = OutletTransformer::transform($merchant->outlets);
+    	if( $this->isRelationshipLoaded($item->merchant, 'outlets') )  {       
+            $merchantOutlets = $item->merchant->outlets->reject(function($outlet) use($item) {
+                return $outlet->id == $item->id;
+            });
+    		$output['outlets'] = OutletTransformer::transform($merchantOutlets);
     	}         	
 
     	return $output;
