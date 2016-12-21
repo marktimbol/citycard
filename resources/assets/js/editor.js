fe08f4e03cc3548e158ecdbdf4b43bf6637f1810
Mark Timbol <mark.timbol@hotmail.com>
2016-12-21 17:33:54 +0400

    Add font family

--- resources/assets/js/editor.js
+++ resources/assets/js/editor.js
@@ -1,5 +1,10 @@
 $(function() {
 	$('#editor').froalaEditor({
-		height: '300px'
+		height: '300px',
+		fontFamily: {
+			"Arial,sans-serif": 'Arial',
+			"ProximaRegular,sans-serif": 'Proxima Nova',
+		},
+		fontFamilySelection: true		
 	})
 });
