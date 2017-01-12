007d040ce900e0d591bccbc069479aeff30c1ce4
Mark Timbol <mark.timbol@hotmail.com>
2017-01-12 15:55:05 +0400

    Apply ItemForReservationTransformer on the Post ItemsForReservationController

--- app/Http/Controllers/Api/Post/ItemsForReservationController.php
+++ app/Http/Controllers/Api/Post/ItemsForReservationController.php
@@ -6,11 +6,13 @@ use App\Post;
 use App\ItemForReservation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Transformers\ItemsForReservationTransformer;
 
 class ItemsForReservationController extends Controller
 {
     public function index(Post $post)
     {
-    	return ItemForReservation::whereTitle($post->title)->first();
+    	$item = ItemForReservation::whereTitle($post->title)->first();
+    	return ItemsForReservationTransformer::transform($item);
     }
 }
