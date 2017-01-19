bcfef78bd9992387b29002e4dcf8bc9e888e2e59
Mark Timbol <mark.timbol@hotmail.com>
2017-01-19 10:28:17 +0400

    User upload profile photo

--- app/Http/Controllers/Api/User/UserPhotosController.php
+++ app/Http/Controllers/Api/User/UserPhotosController.php
@@ -9,6 +9,23 @@ class UserPhotosController extends Controller
 {
     public function store(Request $request)
     {
-    	return $request->all();
+    	$user = auth()->guard('user_api')->user();
+	    $uploadPath = sprintf('users/%s', $user->id);
+
+
+	    if( $file = $request->file->store($uploadPath, 's3') )
+	    {
+	    	$photo = $user->photos()->create([
+	    		'url'	=> $file
+	    	]);
+	    	return response()->json([
+	    		'uploaded'	=> true,
+	    		'photo'	=> $photo
+	    	]);	    	
+	    }
+
+    	return response()->json([
+    		'uploaded'	=> false
+    	]);
     }
 }
