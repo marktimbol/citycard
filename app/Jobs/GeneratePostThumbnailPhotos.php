89e7388df9b916a75fa03c5e4c21a5422f596501
Mark <mark@Marks-MacBook-Pro.local>
2017-02-01 16:35:49 +0400

    Update Generate Post thumbnail photos

--- app/Jobs/GeneratePostThumbnailPhotos.php
+++ app/Jobs/GeneratePostThumbnailPhotos.php
@@ -9,22 +9,23 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class GeneratePostThumbnailPhotos implements ShouldQueue
 {
     use InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $posts;
+    protected $photos;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($posts)
+    public function __construct($photos)
     {
-        $this->posts = $posts;
+        $this->photos = $photos;
     }
 
     /**
@@ -41,20 +42,28 @@ class GeneratePostThumbnailPhotos implements ShouldQueue
                 ->select('id', 'merchant_id')
                 ->first();
 
-            $photo_url = Storage::disk('s3')->get($photo->url);
-            $thumbnail_28_x_28 = Image::make($photo_url)->fit(28,28)->stream();
+            if( Storage::disk('s3')->exists($photo->url) )
+            {
+                Log::info($photo->url .' YEY.');
 
-            $filename = sprintf(
-                'merchants/%s/posts/%s/thumbs/%s',
-                str_slug($post->merchant->name),
-                $post->id,
-                Uuid::uuid1()->toString() . '.jpeg'
-            );
+                $photo_url = Storage::disk('s3')->get($photo->url);
+                $thumbnail_28_x_28 = Image::make($photo_url)->fit(28,28)->stream();
 
-            Storage::disk('s3')->put($filename, $thumbnail_28_x_28->__toString());
+                $filename = sprintf(
+                    'merchants/%s/posts/%s/thumbs/%s',
+                    str_slug($post->merchant->name),
+                    $post->id,
+                    Uuid::uuid1()->toString() . '.jpeg'
+                );
 
-            $photo->thumbnail = $filename;
-            $photo->save();
+                Storage::disk('s3')->put($filename, $thumbnail_28_x_28->__toString());
+
+                $photo->thumbnail = $filename;
+                $photo->save();
+
+            } else {
+                Log::info($photo->url . ' NAH.');
+            }
         }
 
         return 'Done';
