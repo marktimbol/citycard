ccf30ffccd2082c9075f2ef0da6b9122e1740b36
Mark Timbol <mark.timbol@hotmail.com>
2017-02-14 10:53:42 +0400

    Just some comments on the config

--- config/distance.php
+++ config/distance.php
@@ -2,8 +2,10 @@
 
 return [
 
+	// The outlets near the user within 15km
 	'near_outlets'	=> env('NEAR_OUTLETS_DISTANCE', 15), // 15km
 
+	// The clerk should be in this given range in order to Login
 	'outlet_range'	=> env('OUTLET_RANGE', 0.500), // 500m
 
 ];
